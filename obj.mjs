bd7a7f8e9c8738da1fb6fe700181ba6c1720f3ea
ErisEJL <erisel070@gmail.com>
2022-06-15 15:49:04 +0200

    koe

--- obj.mjs
+++ obj.mjs
@@ -2,7 +2,9 @@ export const combine = (a,b,res={}) =>{
     Object.keys(b).forEach(x => typeof b[x] === 'object' && typeof a[x] === 'object'?  b[x] =  {...a[x],...b[x]}:b[x])
     return {...a,...b};
 }
-export const obj = attributes => add => add ? obj(combine(attributes,add)) : attributes;
+export const obj = fn => (attributes) => add => add ? obj(fn)(combine(attributes,add)) : fn ? fn(attributes):attributes;
+
+
 
 export const att = (...keys) => (...values) => {
     let res = {}
--- styles.mjs
+++ styles.mjs
@@ -1,20 +1,6 @@
 import { obj,combine,att } from './obj.mjs';
-
+const setStyle = (o) =>({
+    style:Object.keys(o).map(x => `${x}:${o[x]}`).join(';')
+})
 const color =(h,s,l,a)=> `hsla(${h},${s}%,${l}%,${a})`;
-
-const grid = n => obj({display:"grid"})({"grid-area":n});
-const flex = obj({display:"flex"});
-const center = obj({"align-items":"center","justify-content":"center"});
-const full = obj({"width":"100%",height:"100%"});
-const top = obj({"grid-row":'1/2'});
-const mid = obj({"grid-row":'2/3'});
-const bottom = obj({"grid-row":"3/4"});
-const background = {
-    image:(url,size)=>obj({"background-image":`url(${url})`})({"background-size":size})(),
-    color:(...args)=>obj({"background-color":color(...args)})()
-}
-
-
-const body = {
-    default:(o)=>obj(grid(`10vh 80vh 10vh /50px 1fr 50px`)(center())({height:"100vh",width:"100vw",margin:"0"})(o))()
-};
+const style = obj(setStyle);
