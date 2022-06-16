70a36c84ebb14f21b52c99dc59a84e4f16dc352b
ErisEJL <erisel070@gmail.com>
2022-06-16 12:29:26 +0200

    m

--- el.mjs
+++ el.mjs
@@ -1,22 +1,29 @@
+const ifSingle = el => ['link','meta','input'].includes(el) ? '': `</${el}>`
 const test = {
-            name:"main",
-            attributes:{ class:"koe"},
+    name:"html",
+    innerHTML: [
+        {
+            name:"head",
+            innerHTML:[
+                {
+                    name:"link",
+                    attributes:{
+                        rel:"stylesheet",
+                        href:"https://koe"
+                    }
+                }
+            ]
+        },
+        {
+            name:"body",
             innerHTML:[{
-                name:"div",
-                class:"koe",
-                content:"Hallo"
-            },
-            {
-                name:"span",
-                class:"aap",
-                innerHTML:[{
-                    name:"p",
-                    content:"tata"
-                }]
+                name:"header",
+                content:"hallo"
+            }]
         }
-        ]
+    ]
+}
 
-        }
 
 
 const keys = (obj) => Object.keys(obj);
@@ -28,6 +35,6 @@ const objectify = ((res)=>(keys,values)=>{
 const getAttributes = ob => ob.attributes ? ` ${keys(ob.attributes).map(x => `${x}="${ob.attributes[x]}"`).join(" ")}`:'';
 const getValue = (inc,exc)=>arr=>arr.filter(x =>  exc ? !inc.includes(x):inc.includes(x));
 const getContent = getValue(['listeners','style'],true);
-const element = (obj) => `<${obj.name}>${obj.content}</${obj.name}>`
-const stringify = (obj) => `<${obj.name}${getAttributes(obj)}>${obj.innerHTML ? obj.innerHTML.map(x => stringify(x)).join(''):obj.content}</${obj.name}>`
+
+const stringify = (obj) => `<${obj.name}${getAttributes(obj)}>${obj.innerHTML ? obj.innerHTML.map(x => stringify(x)).join(''):obj.content||''}${ifSingle(obj.name)}`
 console.log(stringify(test))
